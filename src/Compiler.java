ffde3fbd5a4ea4a19c65a2b7d0b79735485cc4f9
Nirton Afonso <nirtonfilho@hotmail.com.br>
2022-09-13 07:49:28 -0300

    Correção primeiro Bug

--- src/Compiler.java
+++ src/Compiler.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 
 public class Compiler {
 
-    private  String textInput = "1.2 new String(22 + 523 - 652 / 241@) * 266\\n";
+    private  String textInput = "1.2 \n +new \n String(22 + 523 - 652 / 241@) * 266\n" +
+            " 25 562 + 555 -655 /55 / / \t";
 
     private char[] text;
     private int next = 0;
@@ -43,27 +44,29 @@ public class Compiler {
                         state = 1;
                         if (!invalidCharacter){
                             tokens.setColumn(column);
-                            joinWord(character);
                         }
+                           joinWord(character);
                     } else if (verify.isMathOperator(character)) {
                         state = 2;
                         if (!invalidCharacter){
                             tokens.setColumn(column);
-                            joinWord(character);
                         }
+                            joinWord(character);
                     } else if (verify.isSymbol(character)) {
                         state = 3;
                         if (!invalidCharacter){
                             tokens.setColumn(column);
-                            joinWord(character);
-                        }else {
+                        }
+                            joinWord(character);}
+
+                        else {
                             state = 0;
                             tokens.setColumn(column);
                             joinWord(character);
                             invalidCharacter = true;
                         }
                         break;
-                    }
+
                 }
                 case 1:{
                     if (verify.isNumber(character)){
@@ -75,11 +78,11 @@ public class Compiler {
                         invalidCharacter = true;
                     }else {
                         if (invalidCharacter) {
-
+                            back();
                             setTokens(TokenName.INVALID_CHARACTER, aux, row);
                             return tokens;
                         }else {
-
+                            back();
                             setTokens(TokenName.NUMBER, aux, row);
                             return tokens;
                         }
@@ -88,7 +91,6 @@ public class Compiler {
                 }
                 case 2:{
                     if (verify.isMathOperator(character)){
-                        state = 2;
                         joinWord(character);
                     } else if (verify.isInvalidCharacter(character)) {
                         state = 2;
@@ -96,11 +98,11 @@ public class Compiler {
                         invalidCharacter = true;
                     }else {
                         if (invalidCharacter){
-
+                            back();
                             setTokens(TokenName.INVALID_CHARACTER, aux, row);
                             return tokens;
                         }else {
-
+                            back();
                             setTokens(TokenName.MATH_OPERATOR, aux, row);
                             return tokens;
                         }
@@ -117,11 +119,11 @@ public class Compiler {
                         invalidCharacter = true;
                     }else {
                         if (invalidCharacter){
-
+                            back();
                             setTokens(TokenName.INVALID_CHARACTER, aux, row);
                             return tokens;
                         }else {
-
+                            back();
                             setTokens(TokenName.SYMBOL, aux, row);
                             return tokens;
                         }
@@ -145,6 +147,10 @@ public class Compiler {
         tokens.setRow(row);
     }
 
+    private void back(){
+        next--;
+        column--;
+    }
 
 
 
